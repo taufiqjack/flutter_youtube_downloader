e1e46dab42c69096a1ea5ff251cc26d29502abc6
taufiqjack <taufiqdwic@gmail.com>
2022-05-19 14:21:26 +0700

    update ui

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,7 +1,8 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_youtube_downloader/flutter_youtube_downloader.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   final GlobalKey<FormState> formKey = GlobalKey();
   final txturl = TextEditingController();
-  String _exstractLink = 'Loading...';
+  String? _exstractLink = 'Loading...';
   String getId = (1).toString().padLeft(2, 'Video00');
 
   @override
@@ -45,36 +46,84 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    String pasteValue = '';
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.3,
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              FontAwesomeIcons.youtube,
+              color: Colors.red,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              'YouTube Downloader',
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
       backgroundColor: Colors.white,
       body: Center(
           child: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Form(
-          key: formKey,
-          child: TextFormField(
-            decoration:
-                const InputDecoration(hintText: 'Masukkan link Youtube disini'),
-            controller: txturl,
-            keyboardType: TextInputType.text,
-            textInputAction: TextInputAction.done,
-            validator: (value) {
-              if (value.toString().isEmpty) {
-                return 'link tidak boleh kosong';
-              }
-              return null;
-            },
-          ),
+        padding: EdgeInsets.only(
+            left: 20, right: 20, top: MediaQuery.of(context).size.height / 3),
+        child: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Masukkan link Youtube disini',
+                  suffixIcon: InkWell(
+                      onTap: () {
+                        FlutterClipboard.paste().then((value) {
+                          setState(() {
+                            txturl.text = value;
+                            pasteValue = value;
+                          });
+                        });
+                      },
+                      child: const Icon(Icons.paste)),
+                ),
+                controller: txturl,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'link tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8), color: Colors.blue),
+              child: TextButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      downloadVideo();
+                    }
+                  },
+                  child: const Text(
+                    'Download',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+          ],
         ),
       )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (formKey.currentState!.validate()) {
-            downloadVideo();
-          }
-        },
-        child: const Icon(Icons.download),
-      ),
     );
   }
 }
